cc8064630817dc46d7e8385d86a7d7c76442d697
EASONGUAN <easonguan97@gmail.com>
2018-11-11 11:27:14 -0500

    Add hog extractor

---
+++ HOGExtractor.py
@@ -0,0 +1,71 @@
+import cv2 as cv
+import numpy as np
+from skimage.feature import hog
+import matplotlib.pyplot as plt
+
+class HOGExtractor:
+
+    def __init__(self, box_size, orientation, pixel_per_cell, cells_per_block, do_trans_sqrt):
+        
+        self.box_size = (box_size, box_size)
+        self.orientation = orientation
+        self.pixel_per_cell = (pixel_per_cell, pixel_per_cell)
+        self.cells_per_block = (cells_per_block, cells_per_block)
+        self.do_trans_sqrt = do_trans_sqrt
+
+
+    def get_HOG(self, image_path):
+
+        image = cv.imread(image_path)
+        image = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+
+        hue = image[:,:,0]
+        saturation = image[:,:,1]
+        lightness = image[:,:,2]
+
+        hue = cv.resize(hue, self.box_size, interpolation = cv.INTER_AREA)
+        saturation = cv.resize(saturation, self.box_size, interpolation=cv.INTER_AREA)
+        lightness = cv.resize(lightness, self.box_size, interpolation=cv.INTER_AREA)
+
+        plt.imshow(lightness)
+
+        hue_features, hue_feature_image = hog(hue,
+                                              orientations=self.orientation,
+                                              pixels_per_cell=self.pixel_per_cell,
+                                              cells_per_block=self.cells_per_block,
+                                              transform_sqrt=self.do_trans_sqrt,
+                                              visualise=True,
+                                              feature_vector=False)
+
+        saturation_features, saturation_feature_image = hog(saturation,
+                                              orientations=self.orientation,
+                                              pixels_per_cell=self.pixel_per_cell,
+                                              cells_per_block=self.cells_per_block,
+                                              transform_sqrt=self.do_trans_sqrt,
+                                              visualise=True,
+                                              feature_vector=False)
+
+        lightness_features, lightness_feature_image = hog(lightness,
+                                              orientations=self.orientation,
+                                              pixels_per_cell=self.pixel_per_cell,
+                                              cells_per_block=self.cells_per_block,
+                                              transform_sqrt=self.do_trans_sqrt,
+                                              visualise=True,
+                                              feature_vector=False)
+
+        output = {'hue_hog': np.reshape(hue_features, -1),
+                  'saturation_hog': np.reshape(saturation_features, -1),
+                  'lightness_hog': np.reshape(lightness_features, -1),
+                  'hue_hog_img': lightness_feature_image,
+                  'saturation_hog_img': saturation_feature_image,
+                  'lightness_hog_img': lightness_feature_image}
+
+        return output
+
+
+
+
+
+
+
+
