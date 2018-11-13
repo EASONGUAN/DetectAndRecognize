3afcdf25496f6e3807310b3d6e9759bac9f50a99
zhengq18 <446994495@qq.com>
2018-11-13 00:47:26 -0500

    Find car

---
+++ FindCar.py
@@ -0,0 +1,66 @@
+import Helper
+from HOGExtractor import HOGExtractor
+from SVM import SVM
+
+import numpy as np
+import cv2 as cv
+import pickle
+import random
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+class CarFinder:
+    def __init__(self, hogExtractor, svm, windowSize, slide_step):
+        # self.windowSize = windowSize
+        self.slide_step = slide_step
+        # self.low_thresh = thresh_low
+        # self.high_thresh = thresh_high
+
+        self.svm = svm
+
+        self.hogExtractor = hogExtractor
+
+
+    def find_car(self, file_name):
+
+        img = cv.imread(file_name)
+
+        self.windowSizeX = int (img.shape[1] / 10)
+        self.windowSizeY = int (img.shape[0] / 10)
+        valid_windows = []
+        break_sig = 0
+        for bottom_left_x in range(self.windowSizeX, img.shape[1], self.slide_step):
+            for bottom_left_y in range(self.windowSizeY, img.shape[0], self.slide_step):
+
+                x = (bottom_left_x - self.windowSizeX, bottom_left_x)
+                y = (bottom_left_y - self.windowSizeY, bottom_left_y)
+
+                if self.window_classfy(x, y, img) == 1:
+                    valid_windows.append((x, y))
+                    # only get the first image
+                    # img = np.copy(img)[y[0]:y[1], x[0]:x[1], :]
+                    # cv.imwrite('test1.jpg', img)
+                    # break_sig = 1
+                    # break
+
+            # if break_sig == 1:
+            #     break
+
+    def window_classfy(self, x, y, img):
+
+        img = np.copy(img)[y[0]:y[1], x[0]:x[1], :]
+
+        if svm.classify(img) == ['positive']:
+            return 1
+        else:
+            return 0
+
+
+svm = SVM("HogData/positive.pickle", "HogData/negative.pickle")
+svm.train_svm()
+extractor = HOGExtractor(64, 9, 8, 2, True)
+
+carFinder = CarFinder(extractor,svm, 64, 16)
+carFinder.find_car('test.jpg')
+
